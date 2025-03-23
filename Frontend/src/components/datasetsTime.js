647b5687f4d88ccdda586754631144e18e524382
Vineeth <113677394+SecretUsrname@users.noreply.github.com>
2025-03-23 23:27:32 +0530

    Update datasetsTime.js

--- Frontend/src/components/datasetsTime.js
+++ Frontend/src/components/datasetsTime.js
@@ -1,21 +1,21 @@
 // Example times for Dataset 1
 export const dataset1Time = [
-    { name: 'Algorithm 1', value: 118.844 },
-    { name: 'Algorithm 2', value: 6479.32 },
-    { name: 'Algorithm 3', value: 300.12 } // Dummy placeholder
+    { name: 'Procedure Clique', value: 49 },
+    { name: 'Bron-Kerbosch', value: 88 },
+    { name: 'Arboricity-Based Algorithm', value: 87619 } // Dummy placeholder
   ];
   
   // Example times for Dataset 2
   export const dataset2Time = [
-    { name: 'Algorithm 1', value: 210.5 },
-    { name: 'Algorithm 2', value: 6400.0 },
-    { name: 'Algorithm 3', value: 500.75 }
+    { name: 'Procedure Clique', value: 65621.6 },
+    { name: 'Bron-Kerbosch', value: 6479 },
+    { name: 'Arboricity-Based Algorithm', value: 272719.8 }
   ];
   
   // Example times for Dataset 3
   export const dataset3Time = [
-    { name: 'Algorithm 1', value: 190.0 },
-    { name: 'Algorithm 2', value: 6599.99 },
-    { name: 'Algorithm 3', value: 1024.5 }
+    { name: 'Procedure Clique', value: 66.2 },
+    { name: 'Bron-Kerbosch', value: 119 },
+    { name: 'Arboricity-Based Algorithm', value: 195106.3 }
   ];
   
